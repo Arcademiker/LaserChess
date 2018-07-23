c3264286effb1172d0e09a96b8efc926efbb2a2e
Thomas <thomas.engels@arcademiker.de>
2018-07-24 00:58:56 +0200

    implemented player attack for grunt
    todo:
    add - unit types special features - in progress
    add - main game loop
    add - ai movement (add enemy unit list)
    add - level stages

--- Units/CGrunt.cpp
+++ Units/CGrunt.cpp
@@ -19,12 +19,32 @@ void CGrunt::do_turn() {
     this->player_options[this->y][std::min(this->x+1,7)] = true;
     this->player_options[std::max(this->y-1,0)][this->x] = true;
     this->player_options[this->y][std::max(this->x-1,0)] = true;
-    // highlight();
-    std::pair<int,int> do_point = this->user_input();
-    while (!this->player_options[do_point.second][do_point.first]) {
+    // todo: highlight();
+    std::pair<int,int> do_point;
+    do {
         do_point = this->user_input();
-    }
-    this->move(do_point.first,do_point.second);
-    /// attack
+    } while (!this->player_options[do_point.second][do_point.first]);
 
+    this->move(do_point.first,do_point.second);
+
+    /// attack
+    /// reset player options
+    this->player_options.clear();
+    this->player_options = std::vector<std::vector<bool>>(size, std::vector<bool>(size, false));
+    CUnit* target1_unit=this->map->get_unit(this->shot(-1,-1));
+    CUnit* target2_unit=this->map->get_unit(this->shot(-1,+1));
+    CUnit* target3_unit=this->map->get_unit(this->shot(+1,-1));
+    CUnit* target4_unit=this->map->get_unit(this->shot(+1,+1));
+    if(target1_unit || target2_unit || target3_unit || target4_unit) {
+        if (target1_unit) { this->player_options[target1_unit->get_y()][target1_unit->get_x()] = true; }
+        if (target2_unit) { this->player_options[target2_unit->get_y()][target2_unit->get_x()] = true; }
+        if (target3_unit) { this->player_options[target3_unit->get_y()][target3_unit->get_x()] = true; }
+        if (target4_unit) { this->player_options[target4_unit->get_y()][target4_unit->get_x()] = true; }
+
+        do {
+            do_point = this->user_input();
+        } while (!this->player_options[do_point.second][do_point.first] || (do_point.first == this->x && do_point.second == this->y));
+
+        this->attack(do_point.first, do_point.second);
+    }
 }
